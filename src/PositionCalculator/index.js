7e9b6c206eb879e3def5921c387a15d7d75ed139
Yash Patel <yashpatel2898@gmail.com>
2023-04-08 20:59:16 +0530

    allow mannual price entry

--- src/PositionCalculator/index.js
+++ src/PositionCalculator/index.js
@@ -12,6 +12,7 @@ const PositionCalculator = () => {
   const [stopLoss, setStopLoss] = useState(0);
   const [lossPerTrade, setLossPerTrade] = useState(0);
   const [positionSize, setPositionSize] = useState(0);
+  const [mannualPrice, setMannualPrice] = useState(false);
 
   const handleInputChangeOnlyNumbers = (event, setFn) => {
     const value = event.target.value;
@@ -101,14 +102,14 @@ const PositionCalculator = () => {
       if (channel !== 'push.ticker') return;
 
       const price = _.get(data, 'data.lastPrice', 0);
-      setLastPrice(price);
+      mannualPrice === false && setLastPrice(price);
     }
 
     return () => {
       socket.close();
     }
 
-  }, [selectedTradingPair])
+  }, [selectedTradingPair, mannualPrice])
 
 
   return (
@@ -162,10 +163,22 @@ const PositionCalculator = () => {
 
 
               <div>
-                <label htmlFor="lastPrice" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Price</label>
-                <input type="text" name="lastPrice" id="lastPrice" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" disabled value={lastPrice || 0}
-                  readOnly
-                ></input>
+                <label htmlFor="lastPrice" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{
+                  mannualPrice ? 'Manual Price' : 'Last Price'
+                }</label>
+                <div className="flex gap-2">
+                  <input type="text" name="lastPrice" id="lastPrice" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value={lastPrice || 0}
+                    onClick={() => setMannualPrice(true)}
+                    onChange={e => { mannualPrice && handleInputChangeOnlyNumbers(e, setLastPrice) }}
+                    readOnly={mannualPrice === false}
+                  ></input>
+                  <button type="button" onClick={() => setMannualPrice(false)} className="text-gray-700 border hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+                    <svg aria-hidden="true" fill="none" className="w-6" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" strokeLinecap="round" strokeLinejoin="round"></path>
+                    </svg>
+                    <span className="sr-only">Icon description</span>
+                  </button>
+                </div>
               </div>
               <div>
                 <label htmlFor="stopLoss" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stop Loss</label>
