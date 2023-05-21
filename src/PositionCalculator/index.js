8f06fc0079e97a2366ff95245f1e15e147f63785
Yash Patel <yashpatel2898@gmail.com>
2023-05-21 15:25:20 +0530

    update value tags

--- src/PositionCalculator/index.js
+++ src/PositionCalculator/index.js
@@ -603,7 +603,8 @@ const PositionCalculator = () => {
                 ></input>
 
                 <div className="mt-1.5">
-                  {_.map([5, 10, 14, 19, 24, 29], (loss) => {
+                  {_.map([1, 2, 3, 5, 9, 14, 21, 29], (loss) => {
+                    // {_.map([5, 10, 14, 19, 24, 29], (loss) => {
                     return (
                       <span
                         className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-300 hover:cursor-pointer hover:bg-gray-200"
