217413efcc21f293c0e8d4385a4195dc1d7f9c3f
Yash Patel <yashpatel2898@gmail.com>
2024-01-29 20:07:00 +0530

    move to Vite⚡🚀🚀🚀

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,6 +57,6 @@ module.exports = {
   plugins: [require("flowbite/plugin")],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
 };
