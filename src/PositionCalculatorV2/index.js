1cf6cabc9fabb5d066bbe008f749002a14edb3b4
Yash Patel <yashpatel2898@gmail.com>
2024-07-03 12:40:33 +0530

    fix settings button size

--- src/PositionCalculatorV2/index.js
+++ src/PositionCalculatorV2/index.js
@@ -25,7 +25,12 @@ import {
   Chip,
 } from "@material-tailwind/react";
 import CustomSelect from "../components/CustomSelect";
-import { CheckIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowDownCircleIcon,
+  CheckIcon,
+  Cog6ToothIcon,
+  DocumentDuplicateIcon,
+} from "@heroicons/react/24/outline";
 
 import getTradeInstance from "../trade";
 import Settings from "../components/Settings";
@@ -329,9 +334,10 @@ const PositionCalculatorV2 = ({
             ></CustomSelect>
 
             <IconButton
-              variant="outlined"
-              className="w-28"
-              size="md"
+              variant="text"
+              className="min-w-11 max-w-full max-h-full w-11 h-11"
+              color="blue-gray"
+              size="lg"
               onClickCapture={(e) => {
                 const modalId = nanoid();
 
@@ -354,18 +360,7 @@ const PositionCalculatorV2 = ({
                 });
               }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                enable-background="new 0 0 24 24"
-                height="24"
-                viewBox="0 0 24 24"
-                width="24"
-              >
-                <g>
-                  <path d="M0,0h24v24H0V0z" fill="none" />
-                  <path d="M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z" />
-                </g>
-              </svg>
+              <Cog6ToothIcon className="h-6 w-6 text-black" />
             </IconButton>
           </div>
 
@@ -397,12 +392,13 @@ const PositionCalculatorV2 = ({
               />
               <Button
                 size="md"
-                color={true ? "gray" : "blue-gray"}
-                disabled={useMarketOrder}
-                className="!absolute right-1 top-1 rounded h-9 flex items-center"
+                variant="text"
+                color="blue-gray"
+                disabled={useMarketPrice}
+                className="!absolute right-1 top-1 rounded h-9 flex items-center px-3"
                 onClick={(e) => setUseMarketPrice(true)}
               >
-                Sync
+                <ArrowDownCircleIcon className="h-5 w-5 text-black" />
               </Button>
             </div>
             <Checkbox
@@ -581,11 +577,13 @@ const PositionCalculatorV2 = ({
               value={`≈ $${positionAmount}`}
               variant="ghost"
               size="sm"
-              className="!absolute right-14 top-[0.63rem] font-light text-gray-700 rounded flex items-center"
+              className="!absolute right-12 top-[0.63rem] font-light text-gray-700 rounded flex items-center"
             />
 
             <IconButton
               size="md"
+              variant="text"
+              color="blue-gray"
               className="!absolute right-1 top-1 rounded h-9 flex items-center"
               onClick={() => {
                 copyToClipboard(positionSize);
@@ -593,9 +591,9 @@ const PositionCalculatorV2 = ({
               }}
             >
               {copied ? (
-                <CheckIcon className="h-4 w-4 text-white" />
+                <CheckIcon className="h-5 w-5 text-black" />
               ) : (
-                <DocumentDuplicateIcon className="h-4 w-4 text-white" />
+                <DocumentDuplicateIcon className="h-5 w-5 text-black" />
               )}
             </IconButton>
           </div>
